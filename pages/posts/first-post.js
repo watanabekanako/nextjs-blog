7e7690424475b6c6bda48c81cec8bac0ce69b671
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2022-08-04 16:38:44 +0900

    コミットしました

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,22 @@
 import Link from "next/link";
-
+import Head from "next/head";
+import Layout from "../../components/layout";
+import styles from "../../components/layout.module.css";
 export default function FirstPost() {
   return (
-    <>
-      <h1>最初の投稿</h1>
-      <h2>
-        <Link href="/">
-          <a>トップページに戻る</a>
-        </Link>
-      </h2>
-    </>
+    <div className={styles.container}>
+      <Layout>
+        <Head>
+          <title>最初の投稿</title>
+        </Head>
+
+        <h1>最初の投稿</h1>
+        <h2>
+          <Link href="/">
+            <a>トップページに戻る</a>
+          </Link>
+        </h2>
+      </Layout>
+    </div>
   );
 }
