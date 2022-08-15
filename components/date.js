60147780dbaee0685b8a2089675104a243695f44
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2022-08-15 14:22:03 +0900

    動的ルーティング追加しました

---
+++ components/date.js
@@ -0,0 +1,11 @@
+import { parseISO, format } from "date-fns";
+import { ja } from "date-fns/locale";
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString);
+  return (
+    <time dateTime={dateString}>
+      {format(date, "PPP (eee)", { locale: ja })}
+    </time>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -76,6 +76,8 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -102,12 +104,21 @@ export default function Home({ allPostsData }) {
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
+            // <li className={utilStyles.listItem} key={id}>
+            //   {title}
+            //   <br />
+            //   {id}
+            //   <br />
+            //   {date}
+            // </li>
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
